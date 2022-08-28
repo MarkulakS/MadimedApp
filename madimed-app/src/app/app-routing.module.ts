6d95bbef0abe5c1dfeca790725ce94a2f30b7b6b
MarkulakS <markulaks17@gmail.com>
2022-08-28 21:34:28 +0200

    Adding Interceptors; Exceptions handling

--- madimed-app/src/app/app-routing.module.ts
+++ madimed-app/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { AuthGuard } from './guards/auth.guard';
 import { ListsComponent } from './lists/lists.component';
 import { MemeberDetailComponent } from './members/memeber-detail/memeber-detail.component';
@@ -18,7 +21,10 @@ const routes: Routes = [
       {path: 'lists', component: ListsComponent },
     ]
   },
-  {path: '**', component: HomeComponent, pathMatch: 'full' }
+  {path: 'errors', component: TestErrorsComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', component: NotFoundComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
--- madimed-app/src/app/app.module.ts
+++ madimed-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { FormsModule } from '@angular/forms';
 
@@ -14,6 +14,10 @@ import { MemeberListComponent } from './members/memeber-list/memeber-list.compon
 import { MemeberDetailComponent } from './members/memeber-detail/memeber-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { SharedModule } from './modules/shared.module';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,9 @@ import { SharedModule } from './modules/shared.module';
     MemeberListComponent,
     MemeberDetailComponent,
     ListsComponent,
+    TestErrorsComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,7 +40,9 @@ import { SharedModule } from './modules/shared.module';
     FormsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
